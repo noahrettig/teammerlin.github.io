980d994aabdbc33eefa3b7d97b63c93f236ada57
Michael Wherry <blackninjamaw1@gmail.com>
2025-03-11 10:07:45 -0500

    Reformat Using Jekyll + Minimal Mistakes Theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Week One - Our First Post!",
+        "excerpt":"Welcome to our blog! This marks the first of many updates as part of our senior inquiry course (CSC-490). Our Week In-Review This week, our team continued development on geo-crop; what we’re calling our Next.js application focused on storing and editing video files for the Geography department. Although we’ve encountered...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/03/09/week-one.html",
+        "teaser": null
+      }]
---
+++ index.md
@@ -0,0 +1,15 @@
+---
+layout: home
+title: Team Merlin's Magical Bird-like Blog
+excerpt: A place for our developers to report their weekly progress!
+header:
+    overlay_color: "#ca74ab99"
+classes:
+    - splash
+    - dark-theme
+---
+
+```typescript
+// Begin intense debugging
+console.log("Hello World!");
+```
